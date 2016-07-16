9045f36bda2bcec5720e6faf25fae91acd1837ee
YuyangMichaelZhou <yyzhou@ucla.edu>
2016-07-16 03:36:41 -0700

    add onCreate and onClick

--- Gocha/app/src/main/java/com/weareonfire/gocha/gocha/CombatModeActivity.java
+++ Gocha/app/src/main/java/com/weareonfire/gocha/gocha/CombatModeActivity.java
@@ -1,13 +1,152 @@
 package com.weareonfire.gocha.gocha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class CombatModeActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.multiplayer.OnInvitationReceivedListener;
+import com.google.android.gms.games.multiplayer.Participant;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
+
+import java.util.ArrayList;
+
+public class CombatModeActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener, RealTimeMessageReceivedListener,
+        RoomStatusUpdateListener, RoomUpdateListener, OnInvitationReceivedListener {
+
+    // Request codes for the UIs that we show with startActivityForResult:
+    final static int RC_SELECT_PLAYERS = 10000;
+    final static int RC_INVITATION_INBOX = 10001;
+    final static int RC_WAITING_ROOM = 10002;
+
+    // Request code used to invoke sign in user interactions.
+    private static final int RC_SIGN_IN = 9001;
+
+    // Client used to interact with Google APIs.
+    private GoogleApiClient mGoogleApiClient;
+
+    // Are we currently resolving a connection failure?
+    private boolean mResolvingConnectionFailure = false;
+
+//    // Has the user clicked the sign-in button?
+//    private boolean mSignInClicked = false;
+
+    // Set to true to automatically start the sign in flow when the Activity starts.
+    // Set to false to require the user to click the button in order to sign in.
+    private boolean mAutoStartSignInFlow = true;
+
+    // Room ID where the currently active game is taking place; null if we're
+    // not playing.
+    String mRoomId = null;
+
+    // Are we playing in multiplayer mode?
+    boolean mMultiplayer = false;
+
+    // The participants in the currently active game
+    ArrayList<Participant> mParticipants = null;
+
+    // My participant ID in the currently active game
+    String mMyId = null;
+
+    // If non-null, this is the id of the invitation we received via the
+    // invitation listener
+    String mIncomingInvitationId = null;
+
+    // Message buffer for sending messages
+    byte[] mMsgBuf = new byte[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combat_mode);
+
+        // Create the Google Api Client with access to Games
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+
+        // set up a click listener for everything we care about
+        for (int id : CLICKABLES) {
+            findViewById(id).setOnClickListener(this);
+        }
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+
+        switch (v.getId()) {
+            case R.id.button_single_player:
+            case R.id.button_single_player_2:
+                // play a single-player game
+//                resetGameVars();
+//                startGame(false);
+                break;
+            case R.id.button_sign_in:
+                // user wants to sign in
+                // Check to see the developer who's running this sample code read the instructions :-)
+                // NOTE: this check is here only because this is a sample! Don't include this
+                // check in your actual production app.
+//                if (!BaseGameUtils.verifySampleSetup(this, R.string.app_id)) {
+//                    Log.w(TAG, "*** Warning: setup problems detected. Sign in may not work!");
+//                }
+//
+//                // start the sign-in flow
+//                Log.d(TAG, "Sign-in button clicked");
+//                mSignInClicked = true;
+                mGoogleApiClient.connect();
+                break;
+            case R.id.button_sign_out:
+                // user wants to sign out
+                // sign out.
+//                Log.d(TAG, "Sign-out button clicked");
+                mSignInClicked = false;
+                Games.signOut(mGoogleApiClient);
+                mGoogleApiClient.disconnect();
+//                switchToScreen(R.id.screen_sign_in);
+                break;
+            case R.id.button_invite_players:
+                // show list of invitable players
+                intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(mGoogleApiClient, 1, 3);
+//                switchToScreen(R.id.screen_wait);
+                startActivityForResult(intent, RC_SELECT_PLAYERS);
+                break;
+            case R.id.button_see_invitations:
+                // show list of pending invitations
+                intent = Games.Invitations.getInvitationInboxIntent(mGoogleApiClient);
+//                switchToScreen(R.id.screen_wait);
+                startActivityForResult(intent, RC_INVITATION_INBOX);
+                break;
+            case R.id.button_accept_popup_invitation:
+                // user wants to accept the invitation shown on the invitation popup
+                // (the one we got through the OnInvitationReceivedListener).
+//                acceptInviteToRoom(mIncomingInvitationId);
+                mIncomingInvitationId = null;
+                break;
+            case R.id.button_quick_game:
+                // user wants to play against a random opponent right now
+//                startQuickGame();
+                break;
+            case R.id.button_click_me:
+                // (gameplay) user clicked the "click me" button
+//                scoreOnePoint();
+                break;
+        }
+    }
+
+
+    final static int[] CLICKABLES = {
+            R.id.button_accept_popup_invitation, R.id.button_invite_players,
+            R.id.button_quick_game, R.id.button_see_invitations, R.id.button_sign_in,
+            R.id.button_sign_out, R.id.button_click_me, R.id.button_single_player,
+            R.id.button_single_player_2
+    };
 }
